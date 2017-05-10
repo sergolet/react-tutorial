8317018062b5f5f5c6847bfd3081330027d26558
sergolet <yurchenko.serg@gmail.com>
2017-05-10 16:34:48 +0300

    remove folder

--- 2-getting-started/app/components/Main/index.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import styles from './styles.js';
-import stylesGlobal from './styles.css';
-
-export default class Application extends Component {
-    render(){
-        return <div className="component" style={styles.primary}>Hello, World!</div>;
-    }
-}
--- 2-getting-started/app/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'; //let React = require('react');
-import ReactDOM from 'react-dom';
-
-import Application from './components/Main';
-
-ReactDOM.render(<Application />, document.getElementById('app'));
